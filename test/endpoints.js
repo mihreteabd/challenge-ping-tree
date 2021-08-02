846784e51617215005e7224754466d0822c1d89b
mihreteabd <mihreteabdemeke1@gmail.com>
2021-08-02 16:29:33 +0300

    test: check route and accept or reject test

--- test/endpoints.js
+++ test/endpoints.js
@@ -3,6 +3,7 @@ process.env.NODE_ENV = 'test'
 var test = require('ava')
 var servertest = require('servertest')
 var map = require('map-limit')
+var querystring = require('querystring')
 
 var server = require('../lib/server')()
 var targets = require('./target-smaple.json')
@@ -87,5 +88,90 @@ test.serial.cb('should update target', function (t) {
     const updatetarget = { ...target, maxAcceptsPerDay: 10 }
 
     stream.end(JSON.stringify(updatetarget))
+  }
+})
+
+test.serial.cb('should route traffic', function (t) {
+  var requests = [
+    {
+      geoState: 'ca',
+      publisher: 'abc',
+      timestamp: '2017-03-12T13:30:00.000Z' // 13hr
+    },
+    {
+      geoState: 'ny',
+      publisher: 'ded',
+      timestamp: '2017-03-12T09:30:00.000Z' // 09 hr
+    },
+    {
+      geoState: 'nv',
+      publisher: 'dedd',
+      timestamp: '2017-03-12T03:30:00.000Z' // 03 hr
+    }
+  ]
+
+  var expected = [
+    'http://example.com',
+    'http://example2.com',
+    'http://example3.com'
+  ]
+
+  map(requests, 1, routeTraffic, function (err, routes) {
+    t.falsy(err, 'should not error')
+    t.deepEqual(routes, expected, 'routes should match')
+    t.end()
+  })
+
+  function routeTraffic (request, cb) {
+    var opts = { encoding: 'json', method: 'GET' }
+
+    var url = '/api/route?' + querystring.stringify(request)
+    servertest(server, url, opts, function (err, res) {
+      if (err) return cb(err)
+      t.is(res.statusCode, 302, 'correct statusCode')
+      cb(null, res.body)
+    })
+  }
+})
+
+test.serial.cb('should correctly handle max traffic', function (t) {
+  var requests = [
+    {
+      geoState: 'mn',
+      publisher: 'abc',
+      timestamp: '2017-03-12T18:30:00.000Z' // 18hr
+    },
+    {
+      geoState: 'mn',
+      publisher: 'abc',
+      timestamp: '2017-03-12T18:30:00.000Z' // 18hr
+    },
+    {
+      geoState: 'mn',
+      publisher: 'abc',
+      timestamp: '2017-03-12T18:30:00.000Z' // 18hr
+    }
+  ]
+
+  var expected = [
+    'http://example5.com',
+    'http://example5.com',
+    { decision: 'reject' } // because the maxAccept is only 2 and this is the third call
+  ]
+
+  map(requests, 1, routeTraffic, function (err, routes) {
+    t.falsy(err, 'should not error')
+    t.deepEqual(routes, expected, 'routes should match')
+    t.end()
+  })
+
+  function routeTraffic (request, cb) {
+    var opts = { encoding: 'json', method: 'GET' }
+    var url = '/api/route?' + querystring.stringify(request)
+    servertest(server, url, opts, function (err, res) {
+      if (err) return cb(err)
+      t.is(res.statusCode, 302, 'correct statusCode')
+      cb(null, res.headers.location || res.body)
+    })
   }
 })
